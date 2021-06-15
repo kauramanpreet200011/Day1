d63066de581aef6eba5b20b8a50ba34ffbb44c65
Amanpreet Kaur <kauramanpreet200011@gmail.com>
2021-06-16 00:02:13 +0530

    Day2

---
+++ Main1.java
@@ -0,0 +1,9 @@
+package com.revature.utils;
+import java.util.Date;
+public class Main1
+{
+    public static void main(String[] args) {
+       Date today= new Date();
+       System.out.println("Todays date is = " +today);
+}
+}
