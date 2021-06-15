5437c46ebf01e570bdef998e6da95bf8b97eaa18
Amanpreet Kaur <kauramanpreet200011@gmail.com>
2021-06-15 23:54:13 +0530

    Day1

---
+++ Details.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Details
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+		String firstName = sc.nextLine();
+		String lastName = sc.nextLine();
+		int age = sc.nextInt();
+		
+		System.out.println("Firstname= " + firstName+ "Lastname= " +lastName+ " age= " +age);
+	}
+}
+
